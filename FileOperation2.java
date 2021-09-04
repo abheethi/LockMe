614a67329e15f2fdbccf03890289b61a76ff38e6
abheethi <71934033+abheethi@users.noreply.github.com>
2021-09-04 15:04:43 +0530

    Add files via upload

--- FileOperation2.java
+++ FileOperation2.java
@@ -1,4 +1,4 @@
-package lockme1;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -10,18 +10,20 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FileOperation2 {
 
-	public static void createMainFolderIfNotPresent(String FileName) {
-	File file = new File(FileName);
-	
-	if(!file.exists()) {
-		file.mkdir();
+	public static void createMainFolderIfNotPresent(String folderName) {
+		File file = new File(folderName);
+
+		// If file doesn't exist, create the main folder
+		if (!file.exists()) {
+			file.mkdirs();
 		}
-	System.out.println( file.getAbsolutePath());
-	}	
-	
+	}
+
 	public static void displayAllFiles(String path) {
 		FileOperation2.createMainFolderIfNotPresent("main");
 		// All required files and folders inside "main" folder relative to current
@@ -94,5 +96,71 @@ public class FileOperation2 {
 		}
 	}
 
-}
+	public static List<String> displayFileLocations(String fileName, String path) {
+		List<String> fileListNames = new ArrayList<>();
+		FileOperation2.searchFileRecursively(path, fileName, fileListNames);
 
+		if (fileListNames.isEmpty()) {
+			System.out.println("\n\n***** Couldn't find any file with given file name \"" + fileName + "\" *****\n\n");
+		} else {
+			System.out.println("\n\nFound file at below location(s):");
+
+			List<String> files = IntStream.range(0, fileListNames.size())
+					.mapToObj(index -> (index + 1) + ": " + fileListNames.get(index)).collect(Collectors.toList());
+
+			files.forEach(System.out::println);
+		}
+
+		return fileListNames;
+	}
+
+	public static void searchFileRecursively(String path, String fileName, List<String> fileListNames) {
+		File dir = new File(path);
+		File[] files = dir.listFiles();
+		List<File> filesList = Arrays.asList(files);
+
+		if (files != null && files.length > 0) {
+			for (File file : filesList) {
+
+				if (file.getName().startsWith(fileName)) {
+					fileListNames.add(file.getAbsolutePath());
+				}
+
+				// Need to search in directories separately to ensure all files of required
+				// fileName are searched
+				if (file.isDirectory()) {
+					searchFileRecursively(file.getAbsolutePath(), fileName, fileListNames);
+				}
+			}
+		}
+	}
+
+	public static void deleteFileRecursively(String path) {
+
+		File currFile = new File(path);
+		File[] files = currFile.listFiles();
+
+		if (files != null && files.length > 0) {
+			for (File file : files) {
+
+				String fileName = file.getName() + " at " + file.getParent();
+				if (file.isDirectory()) {
+					deleteFileRecursively(file.getAbsolutePath());
+				}
+
+				if (file.delete()) {
+					System.out.println(fileName + " deleted successfully");
+				} else {
+					System.out.println("Failed to delete " + fileName);
+				}
+			}
+		}
+
+		String currFileName = currFile.getName() + " at " + currFile.getParent();
+		if (currFile.delete()) {
+			System.out.println(currFileName + " deleted successfully");
+		} else {
+			System.out.println("Failed to delete " + currFileName);
+		}
+	}
+}
--- LockMeMain1.java
+++ LockMeMain1.java
@@ -1,9 +1,15 @@
-package lockme1;
 
 public class LockMeMain1 {
-public static void main(String[] args) {
-	 MenuOptions2.WelcomScreen("LockerMe", "ABHEETHI KOONAL");
-	 FileOperation2.createMainFolderIfNotPresent("Main");
-	 HandleOperation2.handleScreenInput();
-}
+
+	public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperation2.createMainFolderIfNotPresent("main");
+		
+		MenuOptions2.printWelcomeScreen("Simpli learn project", "Abheethi Koonal K T");
+		
+		 HandleOperation2.handleWelcomeScreenInput();
+	}
+
+	
 }
